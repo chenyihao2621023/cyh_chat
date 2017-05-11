8a287432bbd13c60dcf3e5b7b6fc5d5a588b2aca
cyh <372528184@qq.com>
2017-05-11 17:10:29 +0800

    .

--- src/client/socket.js
+++ src/client/socket.js
@@ -1,4 +1,11 @@
 import socketClient from 'socket.io-client';
 import socketWrap from './util/socketWrap';
 
+console.log(process.env.NODE_ENV);
+let url;
+if (process.env.NODE_ENV = 'production') {
+  url = 'http://47.93.30.123:9200'
+}else {
+  url = 'http://localhost:9200/'
+}
 export default socketWrap(socketClient('http://47.93.30.123:9200'));
