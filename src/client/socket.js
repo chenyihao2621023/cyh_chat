7a1d06df6c4b0d05a8e03e88f8656c09690d40fc
cyh <372528184@qq.com>
2017-05-11 17:18:31 +0800

    .

--- src/client/socket.js
+++ src/client/socket.js
@@ -3,9 +3,11 @@ import socketWrap from './util/socketWrap';
 
 console.log(process.env.NODE_ENV);
 let url;
-if (process.env.NODE_ENV = 'production') {
+if (process.env.NODE_ENV === 'production') {
+  console.log('1');
   url = 'http://47.93.30.123:9200'
 }else {
   url = 'http://localhost:9200/'
 }
-export default socketWrap(socketClient('http://47.93.30.123:9200'));
+console.log(url);
+export default socketWrap(socketClient(url));
