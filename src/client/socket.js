adb045cc93295ff0d3709811f987b04e55c267ba
cyh <372528184@qq.com>
2017-05-11 10:17:39 +0800

    .

--- src/client/socket.js
+++ src/client/socket.js
@@ -1,4 +1,4 @@
 import socketClient from 'socket.io-client';
 import socketWrap from './util/socketWrap';
 
-export default socketWrap(socketClient('http://localhost:9200'));
+export default socketWrap(socketClient('http://47.93.30.123:9200'));
