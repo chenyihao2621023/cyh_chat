151f170724b03bc056b916a0d04ea06bda739ada
cyh <372528184@qq.com>
2017-05-11 17:29:37 +0800

    .

--- src/client/socket.js
+++ src/client/socket.js
@@ -1,12 +1,12 @@
 import socketClient from 'socket.io-client';
 import socketWrap from './util/socketWrap';
 
-console.log(process.env.NODE_ENV);
-let url;
-if (process.env.NODE_ENV === 'production') {
-  url = 'http://47.93.30.123:9200'
-}else {
-  url = 'http://localhost:9200/'
-}
-console.log(url);
-export default socketWrap(socketClient(url));
+// console.log(process.env.NODE_ENV);
+// let url;
+// if (process.env.NODE_ENV === 'production') {
+//   url = 'http://47.93.30.123:9200'
+// }else {
+//   url = 'http://localhost:9200/'
+// }
+// console.log(url);
+export default socketWrap(socketClient('http://47.93.30.123:9200'));
