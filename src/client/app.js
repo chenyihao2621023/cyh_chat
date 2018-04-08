7a5b588a0a0f14bbf76c2db502c1d8c5d69430c3
unknown <372528184@qq.com>
2018-04-08 11:58:45 +0800

    1

--- src/client/app.js
+++ src/client/app.js
@@ -2,7 +2,7 @@ import React, { PropTypes } from 'react';
 import { connect } from 'react-redux';
 
 import './app.scss';
-
+// 1
 import user from './action/user';
 import ui from './action/ui';
 import socket from './socket';
